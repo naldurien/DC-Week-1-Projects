31ad95291fbe1ed42e913aa02ab906090f3a90ee
naldurien <nicole.aldurien@gmail.com>
2021-02-05 12:45:48 -0500

    adding friends.py

---
+++ friends.py
@@ -0,0 +1,10 @@
+#Activity 1 - hello-loops.md
+
+friends = ["David", "Courtney", "Lori", "Marty", "JoAnn"]
+for friend in friends:
+    print(friend)
+
+#Katherine's solution
+#for friend in range(0, len(friends)):
+#    print(friends[friend])
+#)
