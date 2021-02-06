c743ea435120a1d15c66f4c6f5631d48bfed0deb
nicole <nicole@darkstar6>
2021-02-05 21:20:30 -0500

    changed neg number message

--- factorial.py
+++ factorial.py
@@ -3,7 +3,7 @@ def factorial():
     number = int(input("Enter a number: "))
     factorial = 1
     if number < 0:
-        print("Factorials cannot be performed on negative numbers.")
+        print("Factorials can't be performed on negative numbers.")
     else:
         for i in range(1, number + 1):
             factorial *= i
