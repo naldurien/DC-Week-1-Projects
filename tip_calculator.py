73b93c99e0fadc91cc33fe7d70ad5d91bd1586e9
nicole <nicole@darkstar6>
2021-02-17 20:53:11 -0500

    added exception handling

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,6 +1,15 @@
-total = input("What is your total?")
-float_total = float(total)
-tip_percent = input("What percentage would you like to tip (as a whole number)?")
-float_tip_percent = float(tip_percent)/100
-tip = float_total * float_tip_percent
-print(f"Your tip amount is ${tip}")
+def tip_calc():
+    try: 
+        total = float(input("\nWhat is your total? "))
+    except:
+        print("\nPlease enter a number with no dollar sign.")
+    try:
+        tip_percent = float(input("\nWhat percentage would you like to tip (as a whole number with no percent sign)? "))
+        decimal_percent = tip_percent/100
+        tip = total * decimal_percent
+        print(f"\nYour tip amount is ${tip:.2f}.\n")
+    except:
+        print("\nSorry, we have encountered an error.\n")
+
+
+tip_calc()
